e51d67f6605acb8dea051a87f91afe9deb4af098
shridhar <shridharanigilkar@gmail.com>
2025-08-09 23:48:03 +0530

    changes in provider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Provider from './provider'
+import Provider from "./Provider";
 import { Toaster } from "sonner"
 import { ThemeProvider } from "next-themes";
 
